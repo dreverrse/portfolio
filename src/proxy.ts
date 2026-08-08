9550db5af82cb179c7918ede9a5307276e5a49ca
dreverrse <63025746+dreverrse@users.noreply.github.com>
2026-08-08 13:42:25 +0700

    Perbaiki CSP agar cover lagu Last.fm bisa tampil

--- src/proxy.ts
+++ src/proxy.ts
@@ -25,7 +25,7 @@ export function proxy() {
         ? "script-src 'self' 'unsafe-inline'"
         : "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "style-src 'self' 'unsafe-inline'",
-      "img-src 'self' https://lastfm.freetls.fastly.net https://cdn.last.fm data: blob:",
+      "img-src 'self' https://lastfm.freetls.fastly.net https://lastfm-img.freetls.fastly.net https://cdn.last.fm data: blob:",
       "font-src 'self'",
       "connect-src 'self'",
       "object-src 'none'",
