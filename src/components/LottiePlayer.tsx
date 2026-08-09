08a5ba67d9e3ea1efacfc443054d7ce16b2e584f
dreverrse <63025746+dreverrse@users.noreply.github.com>
2026-08-09 09:08:30 +0700

    Tambahkan komponen LottiePlayer dan file animasi 404

---
+++ src/components/LottiePlayer.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { DotLottie } from "@lottiefiles/dotlottie-web";
+
+interface LottiePlayerProps {
+  src: string;
+  className?: string;
+}
+
+export default function LottiePlayer({ src, className }: LottiePlayerProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const dotLottie = new DotLottie({
+      canvas: canvasRef.current,
+      src,
+      autoplay: true,
+      loop: true,
+    });
+    return () => {
+      dotLottie.destroy();
+    };
+  }, [src]);
+
+  return <canvas ref={canvasRef} className={className} />;
+}
