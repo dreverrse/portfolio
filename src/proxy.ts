f7f4b297b1ea13dc9491a04f501186604d19eaf1
dreverrse <63025746+dreverrse@users.noreply.github.com>
2026-08-09 14:28:45 +0700

    Izinkan WebAssembly di CSP produksi (wasm-unsafe-eval) untuk animasi Lottie

--- src/proxy.ts
+++ src/proxy.ts
@@ -22,8 +22,8 @@ export function proxy() {
     [
       "default-src 'self'",
       IS_PRODUCTION
-        ? "script-src 'self' 'unsafe-inline'"
-        : "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+        ? "script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'"
+        : "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' https://lastfm.freetls.fastly.net https://lastfm-img.freetls.fastly.net https://cdn.last.fm data: blob:",
       "font-src 'self'",
